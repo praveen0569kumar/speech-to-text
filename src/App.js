8bc5713479e92eb0f59172603f759db27c7ac3f5
praveen0569kumar <praveen0569kumar@gmail.com>
2024-12-28 11:42:46 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
 import Dictaphone from './Dictaphone';
+import Readonly from './components/Readonly';
 
 function App() {
   return (
     <div className="App">
-     <Dictaphone/>
+      <nav>
+        {/* Navigation Links */}
+        
+        <Link to="/readonly">Readonly</Link>
+      </nav>
+
+      {/* Define Routes */}
+      <Routes>
+        
+        <Route path="/" element={<Dictaphone />} />
+        <Route path="/readonly" element={<Readonly />} />
+      </Routes>
     </div>
   );
 }
